71315321651022f4d6953912ce565932a0553b82
leeron ogan-solomon <leeron.solomon-ogan@pau.edu.ng>
2024-10-27 15:44:33 +0100

    (my second commmit)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,10 @@
+fn main(){
+	println!();
+
+	println!("hello ");
+
+	println!("format {} arguments", "some");
+
+	println!("my name is {}. i am the calss rep of steram 1", "Leeron Ogan-solomon");
+
+}
